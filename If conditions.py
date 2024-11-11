cab37b8165aa9deb97ad06f6c35602b127f08209
ciscosky <a.elmahdy.mail@gmail.com>
2024-11-11 06:49:48 +0200

    IF Conditions

---
+++ If conditions.py
@@ -0,0 +1,55 @@
+# Write your code here :-)
+name = 'Mary'
+password = 'swordfish'
+if name == 'Mary':
+    print('Hello, Mary')
+else:
+    print ('Hello stranger')
+
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
+
+
+# print ( 'Enter Your Name plz:')
+# name= input()
+# print ( 'Enter Your Password plz:')
+# Password= input()
+
+
+# if name == 'alice':
+#    print('Hi, Alice.')
+# elif Password < '12':
+#    print('Wlcome Agent Alice :)')
+# else:
+#    print ('Access Deniy, kiddo.')
+
+
+# spam= 0
+# while  spam < 5:
+#    print('Hello, World')
+#    spam= spam + 1
+
+
+# name=''
+# while name != 'your name':
+#    print('Please type your name:')
+#    name = input()
+#    if name== 'your name':
+#        break
+# print('Thank You')
+
+
+# while True:
+#     print('Who are you?')
+#     name= input()
+#     if name != 'joe':
+#         continue
+#     print('Hello joe what is the password? (it is a fish)')
+#     password = input()
+#     if password == 'swordfish':
+#         break
+# print('Access granted')
+
+
