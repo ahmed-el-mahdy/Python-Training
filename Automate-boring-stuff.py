0bf9243c6d8a39f8e2443f88b0ca33222c0c557a
ciscosky <a.elmahdy.mail@gmail.com>
2024-11-11 06:51:57 +0200

    adding more learning files

---
+++ Automate-boring-stuff.py
@@ -0,0 +1,55 @@
+# while True:
+#     print('Who are you?')
+#     name = input()
+#     if name != 'Joe':
+#         continue
+#     print('Hello, Joe. What is the password? (It is a fish.)')
+#     password = input()
+#     if password == 'swordfish':
+#         break
+# print('Access granted.')   
+
+
+# name= ''
+# while not name :
+#     print('Enter your name:')
+#     name=input()
+# print('How many guestes will you have ?')
+# numOfGuests=input()
+# if numOfGuests:
+#     print('Be sure to have enough room for all your guestes')
+#     print('Done')
+
+
+# print('My name is')
+# for i in range (5):
+#     print('jimmy five times ('+ str(i)+')')
+
+          
+# total = 0                   
+# for num in range (101):    # it will cont from the begining and add the num value to total until the num value exced to 100 it will stop
+#     total = total + num
+# print (total)
+
+for i in range(0, 10, 2):  # 2 stand for step forward to move the next value into i
+    print(i)                  # it should print 0 2 4 6 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
