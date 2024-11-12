61b33e31566203d5b0e29072520978748b53517e
ciscosky <a.elmahdy.mail@gmail.com>
2024-11-12 21:12:41 +0200

    Adding a guess game by import random function

--- Automate-boring-stuff.py
+++ Automate-boring-stuff.py
@@ -31,25 +31,44 @@
 #     total = total + num
 # print (total)
 
-for i in range(0, 10, 2):  # 2 stand for step forward to move the next value into i
-    print(i)                  # it should print 0 2 4 6 8
+# for i in range(0, 10, 2):  # 2 stand for step forward to move the next value into i
+#     print(i)                  # it should print 0 2 4 6 8
 
+# import random
+# for i in range(5):
+#     print(random.randint(1, 10)) # Return random integer in range [a, b], including both end points.
 
+# # the same of the above but with better way 
+# from random import * # to import everything from random module 
+# for i in range(5):
+#     print(randint(1,10))
 
+# import sys
+# while True:
+#     print('Type exit to exit')
+#     response = input()
+#     if response == 'exit':
+#         sys.exit()
+#     print('You typed ' + response + '.')
 
+import random
+secretnumber = random.randint(1,20)
+print ('I am thinking of a number between 1 and 20.')
 
+# Ask the player to guess 6 times.
+for guessesTaken in range(1,7):
+    print('Take a guess.')
+    guess= int(input())
 
-
-
-
-
-
-
-
-
-
-
-
-
+    if guess < secretnumber:
+        print('Your guess is too low.')
+    elif guess > secretnumber:
+        print('Your guess is too high.')
+    else:
+        break
+if guess == secretnumber:
+    print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses ! ')
+else:
+    print('Nope. The number I was thinking of was '+ str(secretnumber))
 
 
