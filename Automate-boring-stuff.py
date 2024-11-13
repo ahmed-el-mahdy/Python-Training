3999c34d77f58975e635b3137ef49360ce06d7df
ciscosky <a.elmahdy.mail@gmail.com>
2024-11-13 14:04:19 +0200

    Move forward

--- Automate-boring-stuff.py
+++ Automate-boring-stuff.py
@@ -9,7 +9,6 @@
 #         break
 # print('Access granted.')   
 
-
 # name= ''
 # while not name :
 #     print('Enter your name:')
@@ -20,12 +19,10 @@
 #     print('Be sure to have enough room for all your guestes')
 #     print('Done')
 
-
 # print('My name is')
 # for i in range (5):
 #     print('jimmy five times ('+ str(i)+')')
-
-          
+        
 # total = 0                   
 # for num in range (101):    # it will cont from the begining and add the num value to total until the num value exced to 100 it will stop
 #     total = total + num
@@ -53,9 +50,40 @@
 #         sys.exit()
 #     print('You typed ' + response + '.')
 
+# print('enter a number ^_*')
+# spam = input()
+# if spam == 1:
+#     print('Hello')
+# elif spam == 2:
+#     print('Howdy')
+# else:
+#     print('Greetings!')
 
+# for i in range(1, 11):
+#     print(i)
 
+# i = 1 
+# while i < 11:
+#     print(i)
+#     i= i + 1
 
+def hello():
+    print('Howdy!')
+    print('Howdy!!!!')
+    print('Hello there.')
+hello()
+hello()
+hello()
 
+def hello(name):
+    print('Hello, ' + name)
+    
+hello('alice')
+hello('bob')    
 
+len('hello')
 
+spam = print('hello')
+None == spam
+
+print('cat', 'dog', 'ice', 'sky', sep=',') 
