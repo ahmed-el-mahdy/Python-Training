1ea38c7469b4ba817dad60935e429cfec34ac307
ciscosky <a.elmahdy.mail@gmail.com>
2024-12-24 21:27:07 +0200

    update

--- CharacterPictureGrid.py
+++ CharacterPictureGrid.py
@@ -14,3 +14,4 @@ for col in range(len(grid[0])):  # len(grid[0]) gives the number of columns (6 i
     for row in range(len(grid)):  # len(grid) gives the number of rows (9 in this case)
         print(grid[row][col], end='')  # Print the character at (row, col) without a newline
     print()  # After printing one column, move to the next line
+
